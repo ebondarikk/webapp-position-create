534346028f26020721ec7c327d474b227905e8ca
Eugene B <evgeniy.b@softvoya.com>
2024-11-19 19:59:40 +0300

    use spin

--- src/App.js
+++ src/App.js
@@ -400,7 +400,7 @@ const App = ({tg, categories, bot_id, password, host, user_id, message_id}) => {
         },
       }}
     >
-      <Spin spinning={loader}>
+      <Spin spinning={loader} fullscreen={false} size='large'>
       <div className='form'>
         <div>
           {positions.map(p => (
@@ -415,7 +415,7 @@ const App = ({tg, categories, bot_id, password, host, user_id, message_id}) => {
             ))}
         </div>
         <Button onClick={(addPosition)} className='btn' size='large'>Добавить другой товар</Button>
-        <Button size='large' onClick={save} disabled={loader}>Save</Button>
+        {/* <Button size='large' onClick={save} disabled={loader}>Save</Button> */}
       </div>
       </Spin>
     </ConfigProvider>
